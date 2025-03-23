ec022ebc00361e7519ae4aa67a87a49996695a29
Rameez <rameezhassan.e@gmail.com>
2025-03-23 21:04:19 +0530

    Simplify deployment for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -7,20 +7,21 @@ const nextConfig = {
   },
   output: 'export',
   trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: '/other-stories',
-        destination: '/swahabi-stories',
-        permanent: true,
-      },
-      {
-        source: '/other-stories/:id',
-        destination: '/swahabi-stories/:id',
-        permanent: true,
-      },
-    ]
-  },
+  // Remove redirects since they don't work with static export
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/other-stories',
+  //       destination: '/swahabi-stories',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/other-stories/:id',
+  //       destination: '/swahabi-stories/:id',
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig 
