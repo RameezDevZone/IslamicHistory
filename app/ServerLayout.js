74881c6cc46489a538eea34d58b37a51572233eb
Rameez <rameezhassan.e@gmail.com>
2025-03-23 21:12:54 +0530

    Manual deployment: Clean up and keep only essential files for GitHub Pages

--- app/ServerLayout.js
+++
@@ -1,8 +0,0 @@
-import { metadata } from './metadata'
-import RootLayout from './layout'
-
-export { metadata }
-
-export default function ServerLayout({ children }) {
-  return <RootLayout>{children}</RootLayout>
-} 
